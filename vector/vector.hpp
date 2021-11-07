86ebaaa4b8dda546734d8b0f53cfa75b99d9dabd
Youssef Marji <youssef.marji.contact@gmail.com>
2021-11-07 14:40:42 +0100

    Vector : Cleaning Code View

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,110 +6,20 @@
 /*   By: ymarji <ymarji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 09:49:50 by ymarji            #+#    #+#             */
-/*   Updated: 2021/11/05 11:08:22 by ymarji           ###   ########.fr       */
+/*   Updated: 2021/11/06 14:33:47 by ymarji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 #include <iostream>
-#include <stdexcept>      // std::length_error
+#include <stdexcept>
 #include "enable_if.hpp"
+#include "itarator_traits.hpp"
 // #include <stack>
-#include <iterator>
-#include <vector>
-#include <stack>
-#include <algorithm>
-#include <memory>
 #define put(x) std::cout << x << std::endl
 namespace ft
 {
-	struct input_iterator_tag { };
-	struct output_iterator_tag { };
-	struct forward_iterator_tag : public input_iterator_tag { };
-	struct bidirectional_iterator_tag : public forward_iterator_tag { };
-	struct random_access_iterator_tag : public bidirectional_iterator_tag { };
-	
-	template <class InputIterator1, class InputIterator2>
-		bool equal (InputIterator1 first1, InputIterator1 last1,
-				InputIterator2 first2){
-					while (first1!=last1){
-						if (!(*first1 == *first2))
-							return false;
-					++first1; ++first2;
-					}
-					return true;
-				};
-	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-		bool equal (InputIterator1 first1, InputIterator1 last1,
-				InputIterator2 first2, BinaryPredicate pred){
-					while (first1!=last1){
-						if (!pred(*first1,*first2))
-							return false;
-					++first1; ++first2;
-					}
-					return true;
-				};
-
-	template <class InputIterator1, class InputIterator2>
-		bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
-			InputIterator2 first2, InputIterator2 last2){
-			while (first1!=last1)
-			{
-				if (first2==last2 || *first2<*first1) return false;
-				else if (*first1<*first2) return true;
-				++first1; ++first2;
-			}
-			return (first2!=last2);
-		}
-	template <class InputIterator1, class InputIterator2, class Compare>
-		bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, 
-			InputIterator2 first2, InputIterator2 last2,
-			Compare comp){
-			while (first1!=last1)
-			{
-				if (first2==last2 || comp(*first2, *first1)) return false;
-				else if (comp(*first1, *first2)) return true;
-				++first1; ++first2;
-			}
-			return (first2!=last2);
-		};
-	template<class Iterator>
-	struct iterator_traits
-	{
-		typedef typename Iterator::difference_type difference_type;
-		typedef typename Iterator::value_type value_type;
-		typedef typename Iterator::pointer pointer;
-		typedef typename Iterator::reference reference;
-		typedef typename std::random_access_iterator_tag iterator_category;
-	};
-
-	template<class T>
-	struct iterator_traits<T*>
-	{
-		typedef ptrdiff_t difference_type;
-		typedef T value_type;
-		typedef T* pointer;
-		typedef T& reference;
-		typedef std::random_access_iterator_tag iterator_category;
-	};
-
-
-	template <class Category,			  // iterator::iterator_category
-			  class T,					  // iterator::value_type
-			  class Distance = ptrdiff_t, // iterator::difference_type
-			  class Pointer = T *,		  // iterator::pointer
-			  class Reference = T &		  // iterator::reference
-			>
-	struct iterator
-	{
-			typedef Category iterator_category;
-			typedef T value_type;
-			typedef Distance difference_type;
-			typedef Pointer pointer;
-			typedef Reference reference;
-	};
-
 	template <typename Iter>
 	class	vreverse_iterator: public iterator<typename std::random_access_iterator_tag,
 			typename iterator_traits<Iter>::value_type,
