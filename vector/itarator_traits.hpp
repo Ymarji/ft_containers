f6f65e20abac1c7340d1ebfda8510b674793d6ca
Youssef Marji <youssef.marji.contact@gmail.com>
2021-11-18 14:16:16 +0100

    Iterator : Tag Fixes

--- vector/itarator_traits.hpp
+++ vector/itarator_traits.hpp
@@ -33,7 +33,7 @@ namespace ft{
 		typedef typename Iterator::value_type value_type;
 		typedef typename Iterator::pointer pointer;
 		typedef typename Iterator::reference reference;
-		typedef typename ft::random_access_iterator_tag iterator_category;
+		typedef typename std::random_access_iterator_tag iterator_category;
 	};
 
 	template<class T>
@@ -43,7 +43,7 @@ namespace ft{
 		typedef T value_type;
 		typedef T* pointer;
 		typedef T& reference;
-		typedef ft::random_access_iterator_tag iterator_category;
+		typedef std::random_access_iterator_tag iterator_category;
 	};
 
 	template <class Category,			  // iterator::iterator_category
